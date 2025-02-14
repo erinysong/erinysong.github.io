51c3dde98b0cc0fe0aa04062560ae6db106b027d
Erin Song <121973038+erinysong@users.noreply.github.com>
2025-02-13 19:15:02 -0800

    Rebuild resume pdf

---
+++ build/precache-manifest.08a0dabb390a1fb0776e6b00da2d79a5.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9ab49d516afc53b60652ff56d42dcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27e98295b5badb231096",
+    "url": "/static/css/main.03568216.chunk.css"
+  },
+  {
+    "revision": "3b7aef8acec4d5f19a36",
+    "url": "/static/js/2.d92f162e.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.d92f162e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e98295b5badb231096",
+    "url": "/static/js/main.dee195cf.chunk.js"
+  },
+  {
+    "revision": "dbb34323d7177852dc50",
+    "url": "/static/js/runtime-main.6c5a7bf6.js"
+  },
+  {
+    "revision": "96c1cca1290c30a5707b37d8f74b1bf2",
+    "url": "/static/media/Erin_Song_Resume_2025.96c1cca1.pdf"
+  },
+  {
+    "revision": "4e45740cd9fd864b396bd6cbb7f46313",
+    "url": "/static/media/freight-sans-cmp-bold.4e45740c.ttf"
+  },
+  {
+    "revision": "a7cec56dc06e062dc00878703fe20301",
+    "url": "/static/media/freight-sans-cmp-medium.a7cec56d.ttf"
+  },
+  {
+    "revision": "38f95a7d4a7de842d94b8be9f2762c2c",
+    "url": "/static/media/freight-sans-cmp-reg.38f95a7d.ttf"
+  },
+  {
+    "revision": "92e33a67aa73946ad5c1380b9079a5ec",
+    "url": "/static/media/freight-sans-cmp-semibold.92e33a67.ttf"
+  },
+  {
+    "revision": "90ccccdc6771fcbce62466b93356c32f",
+    "url": "/static/media/freight-sans-cond-bold.90ccccdc.ttf"
+  },
+  {
+    "revision": "99c018c192af181b006d334899cae9ff",
+    "url": "/static/media/freight-sans-cond-medium.99c018c1.ttf"
+  },
+  {
+    "revision": "af93d56b2cdb4fa2e0bdf37adf5e4726",
+    "url": "/static/media/freight-sans-cond-semibold.af93d56b.ttf"
+  },
+  {
+    "revision": "bc448da3057918b36c5ee6ec2d4c74a8",
+    "url": "/static/media/freight-sans-pro-bold.bc448da3.ttf"
+  },
+  {
+    "revision": "daf9dbb83b8d3fb523cb4f9ba78bfcca",
+    "url": "/static/media/freight-sans-pro-book.daf9dbb8.ttf"
+  },
+  {
+    "revision": "4652b1d0832f4fcd6b63bf70c8286412",
+    "url": "/static/media/freight-sans-pro-italic.4652b1d0.ttf"
+  },
+  {
+    "revision": "104806d4a2e428e4959c36c8a0351cea",
+    "url": "/static/media/freight-sans-pro-medium.104806d4.ttf"
+  },
+  {
+    "revision": "c994c44c20a67830aaefd189aa985a8d",
+    "url": "/static/media/freight-sans-pro-semibold.c994c44c.ttf"
+  },
+  {
+    "revision": "570d7c9e362ec4762e71a4789f24744b",
+    "url": "/static/media/freight-sans-pro-xlight.570d7c9e.ttf"
+  },
+  {
+    "revision": "5e20d3b5f59307bca78bfca2c9da87ca",
+    "url": "/static/media/freight-text-pro-bold.5e20d3b5.ttf"
+  },
+  {
+    "revision": "bd6b26073f0f7dba73a866a5ae6ccf56",
+    "url": "/static/media/freight-text-pro-book.bd6b2607.ttf"
+  },
+  {
+    "revision": "2f92cd7811b5c6e235d868f559816b7f",
+    "url": "/static/media/freight-text-pro-medium.2f92cd78.ttf"
+  },
+  {
+    "revision": "a2bd59611a80c0569c93fd0674dab9a8",
+    "url": "/static/media/freight-text-pro-semibold.a2bd5961.ttf"
+  },
+  {
+    "revision": "fa97f78839c8dcb11af105f0be78ec6a",
+    "url": "/static/media/pintos.fa97f788.png"
+  },
+  {
+    "revision": "53f50f173bcb7c680eb69029abcaf683",
+    "url": "/static/media/profile.53f50f17.jpg"
+  },
+  {
+    "revision": "fd54fe36bf933c2057f653244e5bd297",
+    "url": "/static/media/unicornbox.fd54fe36.png"
+  },
+  {
+    "revision": "e750037cb95699bfba2cfb44e82239b6",
+    "url": "/static/media/unloop.e750037c.png"
+  }
+]);
